8e159439bbe55cd361a678e39d71623aabe079f6
codycn-app <codycn2804@gmail.com>
2026-02-15 04:23:56 +0700

    213

--- components/CreatorHeader.tsx
+++
@@ -1,4 +0,0 @@
-
-// Re-export the actual component to maintain compatibility while fixing build errors
-// caused by the file being in the wrong directory with incorrect relative imports.
-export { default } from './creator/CreatorHeader';
